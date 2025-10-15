8700e564ad75ed71d96416a93fac9b7a614ec7b1
Diego Letelier <diegoleteliers10@gmail.com>
2025-10-15 01:19:57 -0300

    Implement global exception filter in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './infrastructure/framework/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Registrar filtro global
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(3000);
 }
 bootstrap();
