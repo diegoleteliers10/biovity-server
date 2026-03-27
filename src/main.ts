f5763c477bb28215646e423f6408cfdd10bdf2f8
Diego Letelier <diegoleteliers10@gmail.com>
2026-03-27 03:13:29 -0300

    fixing back

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import {
   LoggingInterceptor,
